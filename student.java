f3d4881ee7a236d8ab7213fc79971b4fcc6dab33
Aman Tiwari <amantiwari@gmail.com>
2026-04-27 22:13:30 +0530

    student

---
+++ student.java
@@ -0,0 +1,32 @@
+public class student {
+    String name;
+    int a;
+    int b;
+
+    public  student(String name, int a, int b) {
+     this.name=name;
+     this.a=a;
+     this.b=b;
+
+        }
+    public static void main(String args[]) {
+        student s=new  student("aman",4,5);
+        System.out.println(s.name);
+        System.out.println(s.a);
+        System.out.println(s.b);
+
+
+
+
+    }
+
+
+
+
+    }
+
+
+
+
+
+
