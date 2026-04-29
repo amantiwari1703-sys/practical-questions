13d757e88dfd396332fd398ad598dfacb37ada13
Aman Tiwari <amantiwari@gmail.com>
2026-04-29 19:37:33 +0530

    import

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public void area(){
+
+    }
+    public static void main(String[] args){
+
+
+
+
+
+    }
+}
+abstract class Shape{
+    public abstract void area();
+    public void square() {
+        System.out.println("Zzz");
+    }
+
+    }
+
+    class Circle extends Shape{
+    public void rectangle(){
+
+    }
+
+
+    }
--- Vehicle.java
+++ Vehicle.java
@@ -23,6 +23,7 @@ public class Vehicle {
 
             System.out.println("running");
 
+
         }
 
     }
