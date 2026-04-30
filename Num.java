a7c0a1b161e312056ac24abb3043467dc47df7bb
Aman Tiwari <amantiwari@gmail.com>
2026-04-30 07:36:17 +0530

    l

---
+++ Num.java
@@ -0,0 +1,16 @@
+public class Num {
+    public static void main(String[] args){
+        int[]no={1,2,3,4,5,6,7,8,9,10};
+        int sum=10;
+          if(sum>9){
+              System.out.println("max");
+
+          } else {
+              System.out.println("min");
+
+          }
+        for (int a=10;a>=1)
+
+    }
+
+}
